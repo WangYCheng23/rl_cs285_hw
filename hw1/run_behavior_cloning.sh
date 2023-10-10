909ba3c05d88db55bd94d58b4c5f535e1da116fd
WangYCheng23 <ucdcheng@ucdavis.edu>
2023-10-10 22:58:22 +0800

    hw1/.gitignore 	hw2/.gitignore 	hw3/.gitignore 	hw4/.gitignore 	hw5/.gitignore

--- hw1/run_behavior_cloning.sh
+++
@@ -1,5 +0,0 @@
-python cs285/scripts/run_hw1.py \
-	--expert_policy_file cs285/policies/experts/Ant.pkl \
-	--env_name Ant-v2 --exp_name bc_ant --n_iter 1 \
-	--expert_data cs285/expert_data/expert_data_Ant-v2.pkl \
-	--ep_len 1000 --eval_batch_size 5000
